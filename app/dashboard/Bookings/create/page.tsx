c2332cb02532e61cf8240469e5cca678c200256c
Ben <98497711+beanlamachine@users.noreply.github.com>
2024-04-16 22:19:12 -0400

    revalidate page when directed to create new booking to ensure updated values to select from

--- app/dashboard/Bookings/create/page.tsx
+++ app/dashboard/Bookings/create/page.tsx
@@ -1,10 +1,12 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchData } from '@/app/lib/data';
- 
+import { revalidatePath } from 'next/cache';
+
 export default async function Page() {
+  revalidatePath('/dashboard/Bookings/Create');
   const { customers, trains, tickets } = await fetchData();
- 
+
   return (
     <main>
       <Breadcrumbs
