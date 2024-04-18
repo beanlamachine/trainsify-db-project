ac5535942da4fc0763b20605f2f6b9767a61cef6
Ben <98497711+beanlamachine@users.noreply.github.com>
2024-04-17 23:40:33 -0400

    trying to get create booking to load on deployment

--- app/dashboard/Bookings/create/page.tsx
+++ app/dashboard/Bookings/create/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
           { label: 'Bookings', href: '/dashboard/Bookings' },
           {
             label: 'Create Booking',
-            href: '/dashboard/Bookings/Create',
+            href: 'app/dashboard/Bookings/Create',
             active: true,
           },
         ]}
--- app/dashboard/Customers/create/page.tsx
+++ app/dashboard/Customers/create/page.tsx
@@ -1,8 +1,10 @@
 import Form from '@/app/ui/invoices/createForm2';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchData } from '@/app/lib/data';
- 
+import { revalidatePath } from 'next/cache';
+
 export default async function Page() {
+  revalidatePath('/dashboard/Customers/Create');
   const customers = await fetchCustomers();
  
   return (
