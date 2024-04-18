4fda5042e5f602298c1bfc1349d280c4e2dc287f
Ben <98497711+beanlamachine@users.noreply.github.com>
2024-04-17 23:21:01 -0400

    displays train type in tickets

--- app/dashboard/Tickets/page.tsx
+++ app/dashboard/Tickets/page.tsx
@@ -1,4 +1,4 @@
-import { fetchTickets } from '@/app/lib/data';
+import { fetchTickets, fetchData } from '@/app/lib/data';
 import Link from 'next/link';
 import { UpdateCustomer, DeleteCustomer } from '@/app/ui/invoices/buttons.tsx';
 import { revalidatePath } from 'next/cache';
@@ -6,7 +6,8 @@ export default function Page() {
   async function fetchAndRenderData() {
     revalidatePath('/dashboard/Tickets');
     try {
-      const ticketsData = await fetchTickets();
+      //const ticketsData = await fetchTickets();
+      const { customers, trains, tickets } = await fetchData();
       return (
         <main>
           <div>
@@ -18,18 +19,33 @@ export default function Page() {
           <h1>Tickets</h1>
           <br />
           <div>
-            {ticketsData.map((t) => (
-              <div key={t.ticketid}>
-                <p>Ticket's ID: {t.ticketid}</p>
-                <p>Trains's Name: {t.trainid}</p>
-                <p>Origin: {t.origin}</p>
-                <p>Destination: {t.destination}</p>
-                <p>Departure Time: {t.departure_time.toDateString()} {t.departure_time.toTimeString()}</p>
-                <p>Arrive Time: {t.arrival_time.toDateString()} {t.arrival_time.toTimeString()}</p>
-                <p>Available: {t.available}</p>
-                <br />
-              </div>
-            ))}
+            {tickets.map((t) => {
+              const foundTrain = trains.find(
+                (train) => train.trainid === t.trainid,
+              );
+              return (
+                <div key={t.ticketid}>
+                  <p>Ticket's ID: {t.ticketid}</p>
+                  {foundTrain ? (
+                    <p>Trains's Type: {foundTrain.type}</p>
+                  ) : (
+                    <p>Train not found</p>
+                  )}
+                  <p>Origin: {t.origin}</p>
+                  <p>Destination: {t.destination}</p>
+                  <p>
+                    Departure Time: {t.departure_time.toDateString()}{' '}
+                    {t.departure_time.toTimeString()}
+                  </p>
+                  <p>
+                    Arrive Time: {t.arrival_time.toDateString()}{' '}
+                    {t.arrival_time.toTimeString()}
+                  </p>
+                  <p>Available: {t.available}</p>
+                  <br />
+                </div>
+              );
+            })}
           </div>
         </main>
       );
