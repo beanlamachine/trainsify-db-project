e8ed5e9802a2806226c49a645485ceb580545257
Ben <98497711+beanlamachine@users.noreply.github.com>
2024-04-18 14:28:43 -0400

    logo on homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+  return (
+    <div>
+      <p>Dashboard Page</p>
+      <Image 
+        src="/Trainsify_Logo.png"
+        width={500}
+        height={500} 
+        alt="Picture of the author" 
+      />
+    </div>
+  );
+}
