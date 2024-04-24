f8284aca184305f562b3f910a3be7ddcc16eefa8
Armina-Fani <arminafani3250@gmail.com>
2024-04-23 23:55:14 -0400

    fixed booking cancel button

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -118,7 +118,7 @@ export default function Form({ customers, trains, tickets }: FormProps) {
         
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href="/dashboard/Bookings/create"
+          href="/dashboard/Bookings"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
