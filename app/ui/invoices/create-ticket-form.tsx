fbe5d28e3e066cadfb33300ad13298e0693f0f0a
Armina-Fani <arminafani3250@gmail.com>
2024-04-24 00:13:52 -0400

    fixed tickets cancel

--- app/ui/invoices/create-ticket-form.tsx
+++ app/ui/invoices/create-ticket-form.tsx
@@ -148,7 +148,7 @@ export default function Form({ customers, trains, tickets }: FormProps) {
 
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href="/dashboard/Customers"
+          href="/dashboard/Tickets"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
